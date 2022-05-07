e722c7c60c290e0a3a8c282d31612094f9d3d6d3
yangsuwon <ysw7939@gmail.com>
2022-05-08 02:36:54 +0900

    .

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const swaggerDefinition = {
         version: "1.0.0",
         description: "다루 API 명세서",
     },
-    host: "cheeseinthelife.com:433",
+    host: "cheeseinthelife.com:443",
     basePath: "/",
 };
 const options = {
